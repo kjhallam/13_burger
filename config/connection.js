131ab128b2669fc66309ca5e519667ca00d0fe91
KHallam <kjhallam321@gmail.com>
2020-12-09 14:36:35 -0600

    added instuctions on connections

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+
+//===================INSTRUCTIONS ON FILE FOR CONNECTION.JS==========>
+
+// 2. Create a `connection.js` file inside `config` directory.
+
+//    * Inside the `connection.js` file, setup the code to connect Node to MySQL.
+
+//    * Export the connection.
