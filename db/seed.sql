c697139c1f31a3bc246ca8c9cb3137b3f609bebd
KHallam <kjhallam321@gmail.com>
2020-12-10 13:10:52 -0600

    inserted data into the seed file

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon-Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie-Burger', true);
