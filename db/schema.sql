8c79bc12bc9ca6b5fffe56b4a00b9ea60b6eed6a
KHallam <kjhallam321@gmail.com>
2020-12-10 13:09:15 -0600

    created table in schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
 
--- ====================INSTRUCTIONS ON BUILDING SCHEMA FILE===========>
--- 1. Inside your `burger` directory, create a folder named `db`.
+USE burgers_db;
 
--- 2. In the `db` folder, create a file named `schema.sql`. Write SQL queries this file that do the following:
-
---    * Create the `burgers_db`.
---    * Switch to or use the `burgers_db`.
---    * Create a `burgers` table with these fields:
---      * **id**: an auto incrementing int that serves as the primary key.
---      * **burger_name**: a string.
---      * **devoured**: a boolean.
-
--- 3. Still in the `db` folder, create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
-
--- 4. Run the `schema.sql` and `seeds.sql` files into the mysql server from the command line
-
--- 5. Now you're going to run these SQL files.
-
---    * Make sure you're in the `db` folder of your app
+CREATE TABLE burgers
+(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+burger_name VARCHAR(255) NOT NULL,
+devoured BOOLEAN DEFAULT false
+);
