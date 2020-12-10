b243b8b52d78496569e859aaf4fe32f5e0226379
KHallam <kjhallam321@gmail.com>
2020-12-10 13:08:35 -0600

    inserted router to the burger_controller file

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -8,3 +8,40 @@
 //    * `burger.js`
 
 // 4. Create the `router` for the app, and export the `router` at the end of your file.
+
+const express = require('express');
+const router = express.Router();
+const burger = require('../models/burger');
+
+router.get('/', (req, res) => {
+    burger.all(function(data) {
+        let hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    })
+});
+
+router.post('/api/burgers', (req, res) => {
+    burger.create(req.body.burger_name, result => {
+        console.log(result);
+         res.redirect('/');
+    })
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+    burger.update(req.params.id, result => {
+    console.log(result);
+        res.status(200).end();
+    })
+});
+
+router.delete('/api/burgers/:id', (req, res) => {
+    burger.delete(req.params.id, result => {
+        console.log(result);
+        res.status(200).end();
+    })
+});
+
+module.exports = router;
