311a6c97d908a33f6eb896bfe457c9d6090f5fb3
KHallam <kjhallam321@gmail.com>
2020-12-11 20:02:40 -0600

    added text in burger.js file

--- models/burger.js
+++ models/burger.js
@@ -9,3 +9,32 @@
 //     * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
 
 //     * Export at the end of the `burger.js` file.
+const orm = require('../config/orm.js');
+
+const burger = {
+    selectAll: (cb) => {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    insertOne: (cols, vals, cb) => {
+        org.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    updateAll: (objColVals, condition, cb) => {
+        orm.updateAll("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    
+    delete: (condition, cb) => {
+        orm.delete("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.export = burger;
