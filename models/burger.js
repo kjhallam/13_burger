6ff7d55f40eaaeb7577bfbc0634d650839d35f22
KHallam <kjhallam321@gmail.com>
2020-12-14 14:33:50 -0600

    update stuff

--- models/burger.js
+++ models/burger.js
@@ -1,40 +1,24 @@
-//===================INSTRUCTION ON BURGER.JS FILE=====> GOAL
 
-// * Inside your `burger` directory, create a folder named `models`.
-
-//   * In `models`, make a `burger.js` file.
-
-//     * Inside `burger.js`, import `orm.js` into `burger.js`
-
-//     * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
-
-//     * Export at the end of the `burger.js` file.
 const orm = require('../config/orm.js');
 
 const burger = {
-    all: (cb) => {
-        orm.all("burgers", function(res) {
+    selectAll: (cb) => {
+        orm.selectAll("burgers", (res) => {
             cb(res);
         });
     },
 
-    create: (cols, vals, cb) => {
-        org.create("burgers", cols, vals, function(res) {
+    insertOne: (cols, vals, cb) => {
+        orm.insertOne("burgers", cols, vals, (res) => {
             cb(res);
         });
     },
 
-    update: (objColVals, condition, cb) => {
-        orm.update("burgers", objColVals, condition, function(res) {
-            cb(res);
-        });
-    },
-    
-    delete: (condition, cb) => {
-        orm.delete("burgers", condition, function(res) {
+    updateOne: (objColVals, condition, cb) => {
+        orm.updateOne("burgers", objColVals, condition, (res) => {
             cb(res);
         });
     }
 };
 
-module.export = burger;
+module.exports = burger;
