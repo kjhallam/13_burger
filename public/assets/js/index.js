f4d08991676dfc9614d37bf70014157e3195494f
KHallam <kjhallam321@gmail.com>
2020-12-15 13:24:38 -0600

    cleaned up index file- removed comments

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,35 +7,30 @@ $(function() {
         devoured: newDevour
       };
   
-      // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newDevourState
       }).then(
         function() {
           console.log("changed devour to", newDevour);
-          // Reload the page to get the updated list
           location.reload();
         }
       );
     });
   
     $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
       var newBurger = {
         burger_name: {burger_name: $("#brgr").val(), devoured: 0,}
       };
   
-      // Send the POST request.
       $.ajax("/api/burgers", {
         type: "POST",
         data: newBurger
       }).then(
         function() {
           console.log("created new burger");
-          // Reload the page to get the updated list
           location.reload();
         }
       );
