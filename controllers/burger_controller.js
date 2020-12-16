53216dc6566bc02ddd4e33e19fb81cfd6d60c345
KHallam <kjhallam321@gmail.com>
2020-12-15 22:07:53 -0600

    correct labeling

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res) {
 });
 
 router.post('/api/burgers', function(req, res) {
+    console.log(req.body);
     burger.insertOne([
         'burger_name', "devoured"
     ], [
